68e11898c9d5cd269cbf69efdb8a07473382c2a8
Sam Gruse <sgruse89@gmail.com>
2016-08-08 16:26:51 -0700

    Initialized toggle side nav

--- src/components/nav/nav-controller.js
+++ src/components/nav/nav-controller.js
@@ -5,6 +5,18 @@
     ]);
     function NavBarController() {
       var vm = this;
+
+      vm.dropDown = false;
+      vm.toggleDrop = function() {
+        console.log('TOGGLE DROP HIT');
+        if (!vm.dropDown) {
+          console.log('IF HAS BEEN HIT');
+          vm.dropDown = true;
+        }
+        else {
+          vm.dropDown = false;
+        }
+      }
     };
 
 })();
